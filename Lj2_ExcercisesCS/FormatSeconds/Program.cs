a15f6b5000f3ae7e6b49c87827498020d7475bb6
IvoDeK <ivodoetyt@gmail.com>
2020-02-11 15:39:01 +0100

    Ivo - FormatSeconds
    Ivo - FormatSeconds
    
    Made input requester to request a number from the user. When it's not an number or a number below 1 then it will request a new input.

--- Lj2_ExcercisesCS/FormatSeconds/Program.cs
+++ Lj2_ExcercisesCS/FormatSeconds/Program.cs
@@ -15,6 +15,20 @@ namespace FormatSeconds
     {
         static void Main(string[] args)
         {
+            //Save the user input
+            int userInput = 0;
+            
+            //Get the user input until they give a number
+            do
+            {
+                //Tell user to input a number
+                Console.WriteLine("Please insert the amount of seconds you want to format.");
+                //Try to parse the input the users gives and output to userInput
+                int.TryParse(Console.ReadLine(), out userInput);
+                
+                //If User inserted no number or a number below 1 then tell them to retry
+                if(userInput <= 0) Console.WriteLine("Oops you didn't insert a number that I can format.\n");
+            } while (userInput > 0);
         }
     }
 }
