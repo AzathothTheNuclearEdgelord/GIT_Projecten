01c211b5489df3eb055da9ca99295f99110755cb
LuukHendriks99 <luukhendriks2@gmail.com>
2020-02-11 16:09:10 +0100

    RockPaperScissors.cs
    Added: everything

--- Lj2_ExcercisesCS/RockPaperScissors/Program.cs
+++ Lj2_ExcercisesCS/RockPaperScissors/Program.cs
@@ -15,6 +15,54 @@ namespace RockPaperScissors
     {
         static void Main(string[] args)
         {
+            //array Rock, Paper, Scissors
+            string[] rockPaperScissors = new string[3];
+            rockPaperScissors[0] = "Rock";
+            rockPaperScissors[1] = "Paper";
+            rockPaperScissors[2] = "Scissors";
+
+            //Random
+            Random rndm = new Random();
+
+            //choice AI
+            int choiceAI;
+
+            //user input
+            string userInput;
+          
+            //choice player
+            int choice;
+
+            //continue Rock Paper Scissors input do-while loop
+            bool resume = true;
+
+            //introduction
+            Console.WriteLine("We've gathered here today to play Rock, Paper, Scissors... AGAINST A COMPUTER!");
+            Console.WriteLine("Here we go...");
+            Console.ReadLine();
+
+            //input Rock, Paper or Scissors
+            do
+            {
+                Console.Clear();
+                resume = true;
+                Console.WriteLine("Player, type your choice: Rock - Paper - Scissors");
+                userInput = Console.ReadLine();
+                Console.WriteLine();
+                if(userInput != "Rock")
+                {
+                    resume = true;
+                }
+                else
+                {
+                    resume = false;
+                }
+            } while (resume = true);
+
+           
+
+            
+
         }
     }
 }
